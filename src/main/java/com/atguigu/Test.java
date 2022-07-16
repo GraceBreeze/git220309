e8f6984782540e4116209c3dccb1d5d0dae9039e
GraceBreeze <GraceBreeze@163.com>
2022-07-16 10:54:22 +0800

    second commit

--- src/main/java/com/atguigu/Test.java
+++ src/main/java/com/atguigu/Test.java
@@ -7,5 +7,6 @@ package com.atguigu;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello Git !");
+        System.out.println("Hello world");
     }
 }
